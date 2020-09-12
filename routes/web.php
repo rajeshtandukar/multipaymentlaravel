dd1a4e1207a1e7488ec0f0ecdb58e432fc12ec73
Rajesh <rtandukar@gmail.com>
2020-09-12 08:14:18 +0545

    Routes

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+
+Route::get('/', 'ProductController@list')->name('home');
+Route::post('checkout', 'OrderController@checkout')->name('checkout');
+
+Route::any('esewa/success', 'EsewaController@success')->name('esewa.success');
+Route::any('esewa/fail', 'EsewaController@fail')->name('esewa.fail');
+Route::get('payment/response', 'EsewaController@payment_response')->name('payment.response');
+
+Route::any('fonepay/return', 'FonepayController@fonepay_response')->name('fonepay.return');
+
+
